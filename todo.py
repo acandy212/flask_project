5aff12c35ec53a3f2659b18e892d07b172d33780
acandy212 <candies_amanda@columbusstate.edu>
2022-09-24 00:43:43 +0000

    added html page

--- todo.py
+++ todo.py
@@ -1,11 +1,22 @@
 #app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template('home.html')
+
+@app.route('/addTodo', method={'POST'})
+def addTodo():
+    #access body information
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+#With GET, PUT, POST, DELETE you can pass information through the query and path parameters
+#POST AND PUT the information will be passed within the body
